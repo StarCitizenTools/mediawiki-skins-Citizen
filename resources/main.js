7046985a09a300558e3382f88f97abad82ed2c2b
alistair3149 <alistair3149@users.noreply.github.com>
2016-12-09 05:42:28 -0500

    fix js attempt #1

--- resources/main.js
+++ resources/main.js
@@ -1,16 +1,14 @@
 /* JavaScript for the Citizen skin */
 
-(function () {
-    /* Register service worker */
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
-            navigator.serviceWorker.register('../../sw.js').then(function (registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }).catch(function (err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
+/* Register service worker */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('../../sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
         });
-    }
-});
+    });
+}
