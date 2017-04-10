a2abf045a9855d92a8a6a0e0fbd149d942906747
alistair3149 <alistair3149@users.noreply.github.com>
2017-04-10 18:39:09 -0400

    Removed ServiceWorker until it works

--- resources/main.js
+++ resources/main.js
@@ -1,14 +1,1 @@
 /* JavaScript for the Citizen skin */
-
-/* Register service worker */
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('../../sw.js').then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
