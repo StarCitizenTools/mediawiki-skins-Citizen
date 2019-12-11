5713e89482648b4d88232c8ce405caa91189feaa
alistair3149 <alistair31494322@gmail.com>
2019-12-11 18:48:36 -0500

    Fixed a stupid oversight

--- resources/scripts/typeahead-init.js
+++ resources/scripts/typeahead-init.js
@@ -9,7 +9,7 @@
 
 	var inputEvent,
 		searchInput = document.getElementById( 'search-input' ),
-		typeAhead = new WMTypeAhead( 'search-form', 'searchInput' );
+		typeAhead = new WMTypeAhead( 'search-form', 'search-input' );
 
 	/**
 	 * Testing for 'input' event and falling back to 'propertychange' event for IE.
