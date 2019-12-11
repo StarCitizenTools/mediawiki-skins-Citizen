e749a5c33707143cfb01303be5f1e3db06ae68b2
alistair3149 <alistair31494322@gmail.com>
2019-12-11 16:38:01 -0500

    Additional search tweaks

--- includes/CitizenTemplate.php
+++ includes/CitizenTemplate.php
@@ -292,8 +292,8 @@ class CitizenTemplate extends BaseTemplate {
 			]
 		);
 		$html .= Html::hidden( 'title', $this->get( 'searchtitle' ) );
-		$html .= Html::label( $this->getMsg( 'search' )->text(), 'search-input' );
-		$html .= $this->makeSearchInput( [ 'id' => 'search-input' ] );
+		$html .= Html::label( $this->getMsg( 'search' )->text(), 'search-input', 'class' => 'screen-reader-text' );
+		$html .= $this->makeSearchInput( [ 'id' => 'search-input', 'type' => 'search' ] );
 		$html .= $this->makeSearchButton( 'go', [ 'id' => 'search-button' ] );
 		$html .= Html::closeElement( 'form' );
 
