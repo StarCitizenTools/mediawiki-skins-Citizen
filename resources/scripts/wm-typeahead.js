65a524d40cb4ec16b0de2b3ac6d12226ddd6864f
alistair3149 <alistair31494322@gmail.com>
2019-12-11 22:17:40 -0500

    Use global var for search  #2

--- resources/scripts/wm-typeahead.js
+++ resources/scripts/wm-typeahead.js
@@ -195,7 +195,7 @@ window.WMTypeAhead = function(appendTo, searchInput) { // eslint-disable-line no
 
         // Change sitename here
         // TODO: Make it configurable from the skin
-        hostname = '//' + wgServer + wgScriptPath + '/api.php?';
+        hostname = wgServer + wgScriptPath + '/api.php?';
 
         // If script already exists, remove it.
         if (script) {
@@ -332,7 +332,7 @@ window.WMTypeAhead = function(appendTo, searchInput) { // eslint-disable-line no
             // TODO: Make it configurable from the skin
             suggestionLink = mw.html.element('a', {
                 'class': 'suggestion-link',
-                href: 'https://' + wgServer + '/' + encodeURIComponent(page.title.replace(/ /gi, '_'))
+                href: wgServer + '/' + encodeURIComponent(page.title.replace(/ /gi, '_'))
             }, new mw.html.Raw(suggestionText + suggestionThumbnail));
 
             string += suggestionLink;
