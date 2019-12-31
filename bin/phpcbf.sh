b0da15629031d6c469f2ea3e0396f304b6a1648a
alistair3149 <alistair31494322@gmail.com>
2019-12-30 19:04:02 -0500

    Force phpcbf to exit with 0

--- bin/phpcbf.sh
+++ bin/phpcbf.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
-# Wrap phpcbf to turn 1 success exit code into 0 code.
+# Force phpcbf to exit with zero
 # See https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927
 
 root=$( dirname $0 )/..
 
-$root/vendor/bin/phpcbf $@
-exit=$?
+$root/vendor/bin/phpcbf
 
-echo $?
-
-# Exit code 1 is used to indicate that all fixable errors were fixed correctly.
-if [[ $exit == 1 ]]; then
-	exit=0
-fi
-
-exit $exit
+exit 0
