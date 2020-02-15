39ee1ee2b3f8829e5557fefb719a45f52d6d7871
alistair3149 <alistair31494322@gmail.com>
2020-02-15 17:13:26 -0500

    Disabled ESlint cache

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function ( grunt ) {
 			options: {
 				reportUnusedDisableDirectives: true,
 				extensions: [ '.js', '.json' ],
-				cache: true,
 				fix: true
 			},
 			all: [
