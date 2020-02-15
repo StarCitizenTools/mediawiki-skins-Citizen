d8320d8ca1b2515d7d597cf1a908bdf7d47f90e3
alistair3149 <alistair31494322@gmail.com>
2020-02-15 17:11:46 -0500

    Disabled force for eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,8 +12,7 @@ module.exports = function ( grunt ) {
 				reportUnusedDisableDirectives: true,
 				extensions: [ '.js', '.json' ],
 				cache: true,
-				fix: true,
-				force: true
+				fix: true
 			},
 			all: [
 				'**/*.js{,on}',
