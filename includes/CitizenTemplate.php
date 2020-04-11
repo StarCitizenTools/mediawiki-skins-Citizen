1e620b208e16a2f148c4f4987f6058aeb0cd4f82
alistair3149 <alistair31494322@gmail.com>
2020-04-11 19:43:07 -0400

    Attempt to fix the syntax again3

--- includes/CitizenTemplate.php
+++ includes/CitizenTemplate.php
@@ -24,7 +24,6 @@ class CitizenTemplate extends BaseTemplate {
 
 		// TODO: Convert the rest to Mustache
 		ob_start();
-		?>
 		$loggedInClass = 'not-logged';
 
 		// Add class if logged in
@@ -45,7 +44,6 @@ class CitizenTemplate extends BaseTemplate {
 		$html .= $this->getTrail();
 
 		echo $html;
-		<?php
 		$params['html-unported'] = ob_get_contents();
 		ob_end_clean();
 
