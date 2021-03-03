527692cd99c914ab770a63f29b01d079ac223245
alistair3149 <alistair31494322@gmail.com>
2021-03-02 19:13:50 -0500

    fix: fix various javascript error

--- resources/skins.citizen.scripts.collapsiblesections/collapsibleSections.js
+++ resources/skins.citizen.scripts.collapsiblesections/collapsibleSections.js
@@ -1,21 +1,19 @@
-
+function bindClick( collToggle, collSection, i, j ) {
+	return function () {
+		j = i + 1;
+		this.classList.toggle( 'section-toggle--collapsed' );
+		collSection[ j ].classList.toggle( 'section-collapsible--collapsed' );
+	};
+}
 
 function main() {
 	var collSection = document.getElementsByClassName( 'section-collapsible' ),
 		collToggle = document.getElementsByClassName( 'section-toggle' ),
 		i, j;
 
-	for (i = 0; i < collToggle.length; i++) {
-		collToggle[i].addEventListener("click", bindClick( collToggle, collSection, i));
+	for ( i = 0; i < collToggle.length; i++ ) {
+		collToggle[ i ].addEventListener( 'click', bindClick( collToggle, collSection, i, j ) );
 	}
-}
-
-function bindClick( collToggle, collSection, i ) {
-	return function() {
-		j = i + 1;
-        this.classList.toggle( 'section-toggle--collapsed' );
-		collSection[j].classList.toggle( 'section-collapsible--collapsed' );
-    };
 }
 
 main();
