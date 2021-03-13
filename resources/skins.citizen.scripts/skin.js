1b3fff4170d631a150003214faa95b35292d95a1
alistair3149 <alistair31494322@gmail.com>
2021-03-13 12:43:28 -0500

    fix: CSS animation flash on page load on Chromium

--- resources/skins.citizen.scripts/skin.js
+++ resources/skins.citizen.scripts/skin.js
@@ -3,12 +3,46 @@ var themeToggle = require( './themeToggle.js' ),
 	checkboxHack = require( './checkboxHack.js' );
 
 /**
+ * Based on Vector
+ * Wait for first paint before calling this function. That's its whole purpose.
+ *
+ * Some CSS animations and transitions are "disabled" by default as a workaround to this old Chrome
+ * bug, https://bugs.chromium.org/p/chromium/issues/detail?id=332189, which otherwise causes them to
+ * render in their terminal state on page load. By adding the `vector-animations-ready` class to the
+ * `html` root element **after** first paint, the animation selectors suddenly match causing the
+ * animations to become "enabled" when they will work properly. A similar pattern is used in Minerva
+ * (see T234570#5779890, T246419).
+ *
+ * Example usage in Less:
+ *
+ * ```less
+ * .foo {
+ *     color: #f00;
+ *     .transform( translateX( -100% ) );
+ * }
+ *
+ * // This transition will be disabled initially for JavaScript users. It will never be enabled for
+ * // no-JS users.
+ * .citizen-animations-ready .foo {
+ *     .transition( transform 100ms ease-out; );
+ * }
+ * ```
+ *
+ * @param {Document} document
  * @return {void}
  */
-function main() {
+function enableCssAnimations( document ) {
+	document.documentElement.classList.add( 'citizen-animations-ready' );
+}
+
+/**
+ * @return {void}
+ */
+function main( window ) {
+	enableCssAnimations( window.document );
 	themeToggle.init();
 	search.init();
 	checkboxHack.init();
 }
 
-main();
+main( window );
