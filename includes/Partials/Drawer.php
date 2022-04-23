a8485ac76a488b39f765a3e05a705bb828ecd261
alistair3149 <alistair31494322@gmail.com>
2022-04-23 01:31:22 -0400

    fix: remove unused debug code

--- includes/Partials/Drawer.php
+++ includes/Partials/Drawer.php
@@ -160,46 +160,6 @@ final class Drawer extends Partial {
 	}
 
 	/**
-	 * Use to convert large positive numbers in to short form like 1K+, 100K+, 199K+, 1M+, 10M+, 1B+ etc
-	 * 
-	 * @param $n
-	 * @param $precision d
-	 * @return string
-	 */
-	private function number_format_short( $n, $precision = 1 ) {
-		if ($n < 900) {
-			// 0 - 900
-			$n_format = number_format($n, $precision);
-			$suffix = '';
-		} else if ($n < 900000) {
-			// 0.9k-850k
-			$n_format = number_format($n / 1000, $precision);
-			$suffix = 'K';
-		} else if ($n < 900000000) {
-			// 0.9m-850m
-			$n_format = number_format($n / 1000000, $precision);
-			$suffix = 'M';
-		} else if ($n < 900000000000) {
-			// 0.9b-850b
-			$n_format = number_format($n / 1000000000, $precision);
-			$suffix = 'B';
-		} else {
-			// 0.9t+
-			$n_format = number_format($n / 1000000000000, $precision);
-			$suffix = 'T';
-		}
-
-	  // Remove unecessary zeroes after decimal. "1.0" -> "1"; "1.00" -> "1"
-	  // Intentionally does not affect partials, eg "1.50" -> "1.50"
-		if ( $precision > 0 ) {
-			$dotzero = '.' . str_repeat( '0', $precision );
-			$n_format = str_replace( $dotzero, '', $n_format );
-		}
-
-		return $n_format . $suffix;
-	}
-
-	/**
 	 * Add a link to special pages and the upload form to the first portal in the drawer
 	 *
 	 * @param string &$htmlItems
