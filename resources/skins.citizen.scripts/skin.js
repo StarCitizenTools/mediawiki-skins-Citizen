96013bb126797e8ab5db2588979884946e7185e5
alistair3149 <alistair31494322@gmail.com>
2022-04-25 22:27:52 -0400

    feat: deprecate --background-color-dp-XX in favor of --color-surface-X
    --background-color-dp-XX is now fully deprecated in favor of a simpler 4 color surface system. It can be accessed through the --color-surface-X CSS var.

--- resources/skins.citizen.scripts/skin.js
+++ resources/skins.citizen.scripts/skin.js
@@ -126,11 +126,6 @@ function main( window ) {
 	}, false );
 
 	mw.loader.load( 'skins.citizen.preferences' );
-
-	// Depreciation messages
-	// TODO: Remove when hard depreciated
-	mw.log.warn( 'CITIZEN: --background-color-dp-XX is depreciated; use --color-surface-X instead.' );
-	mw.log.warn( 'CITIZEN: Security headers will be depreciated in the next version.' );
 }
 
 main( window );
