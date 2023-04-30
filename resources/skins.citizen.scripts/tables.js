e45af9bbbeffe40f195b148d7d7b78b02b34cddd
alistair3149 <alistair31494322@gmail.com>
2023-04-30 19:54:31 -0400

    feat(core): ✨ add scroll affordance to tables

--- resources/skins.citizen.scripts/tables.js
+++ resources/skins.citizen.scripts/tables.js
@@ -1,4 +1,59 @@
 /**
+ * Set up scroll affordance for an overflowed element
+ * TODO: Move this out of tables when this is used by more stuff
+ *
+ * @param {HTMLElement} element
+ * @return {void}
+ */
+function setupOverflowState( element ) {
+	const initState = () => {
+		const updateState = () => {
+			const
+				containerWidth = element.parentNode.offsetWidth,
+				contentWidth = element.scrollWidth;
+
+			// Break if no horizontal overflow
+			if ( contentWidth <= containerWidth ) {
+				element.parentNode.classList.remove( 'citizen-overflow--left' );
+				element.parentNode.classList.remove( 'citizen-overflow--right' );
+				return;
+			}
+
+			const currentPosition = element.parentNode.scrollLeft;
+
+			if ( currentPosition <= 0 ) {
+				// At the start
+				element.parentNode.classList.remove( 'citizen-overflow--left' );
+				element.parentNode.classList.add( 'citizen-overflow--right' );
+			} else if ( currentPosition + containerWidth >= contentWidth ) {
+				// At the end
+				element.parentNode.classList.remove( 'citizen-overflow--right' );
+				element.parentNode.classList.add( 'citizen-overflow--left' );
+			} else {
+				// At the middle
+				element.parentNode.classList.add( 'citizen-overflow--left' );
+				element.parentNode.classList.add( 'citizen-overflow--right' );
+			}
+		};
+
+		updateState();
+
+		// Update state on element scroll
+		element.parentNode.addEventListener( 'scroll', () => {
+			window.requestAnimationFrame( updateState );
+		} );
+	};
+
+	initState();
+
+	// Listen for window resize
+	if ( window.ResizeObserver ) {
+		const overflowResizeObserver = new ResizeObserver( mw.util.debounce( 250, initState ) );
+		overflowResizeObserver.observe( element );
+	}
+}
+
+/**
  * Wrap table in div container to make it scrollable without breaking layout
  *
  * @param {HTMLTableElement} table
@@ -9,6 +64,8 @@ function wrapTable( table ) {
 	wrapper.classList.add( 'citizen-table-wrapper' );
 	table.parentNode.insertBefore( wrapper, table );
 	wrapper.appendChild( table );
+
+	setupOverflowState( table );
 }
 
 /**
@@ -20,8 +77,7 @@ function init( bodyContent ) {
 		return;
 	}
 
-	const
-		tables = bodyContent.querySelectorAll( 'table' );
+	const tables = bodyContent.querySelectorAll( 'table' );
 
 	tables.forEach( ( table ) => {
 		wrapTable( table );
