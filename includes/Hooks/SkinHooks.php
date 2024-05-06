5d59a6d3c0ee46f9b3362b4499c82aa51f3a11f8
alistair3149 <alistair31494322@gmail.com>
2024-05-06 14:06:04 -0400

    feat(core): ✨ enable caching for inline JS

--- includes/Hooks/SkinHooks.php
+++ includes/Hooks/SkinHooks.php
@@ -68,8 +68,7 @@ class SkinHooks implements
 		if ( $this->getConfigValue( 'CitizenEnablePreferences', $out ) === true ) {
 			$script = file_get_contents( MW_INSTALL_PATH . '/skins/Citizen/resources/skins.citizen.scripts/inline.js' );
 			$script = Html::inlineScript( $script );
-			// TODO: Consider turning on cache after this is more stable
-			$script = RL\ResourceLoader::filter( 'minify-js', $script, [ 'cache' => false ] );
+			$script = RL\ResourceLoader::filter( 'minify-js', $script );
 			$out->addHeadItem( 'skin.citizen.inline', $script );
 		}
 	}
