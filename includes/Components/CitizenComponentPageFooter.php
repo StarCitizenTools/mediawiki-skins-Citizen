9a6b2e48614865ff89b11b2a7d84bc5a630e3b79
WaitSpring <me@waitspring.com>
2024-11-05 12:57:53 +0800

    fix(PageFooter): 🐛 remove label from page footer

--- includes/Components/CitizenComponentPageFooter.php
+++ includes/Components/CitizenComponentPageFooter.php
@@ -35,11 +35,13 @@ class CitizenComponentPageFooter implements CitizenComponent {
 	public function getTemplateData(): array {
 		$footerData = $this->footerData;
 
+		/*
 		// Add label to footer-info to use in PageFooter
 		foreach ( $footerData['array-items'] as &$item ) {
 			$msgKey = 'citizen-page-info-' . $item['name'];
 			$item['label'] = $this->localizer->msg( $msgKey )->text();
 		}
+		*/
 
 		return $footerData;
 	}
