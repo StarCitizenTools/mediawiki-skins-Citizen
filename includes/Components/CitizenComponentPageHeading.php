504c2373a080a91e1023eb1858fbd63433e07989
alistair3149 <alistair31494322@gmail.com>
2024-12-11 16:32:41 -0500

    fix(pageHeading): 🐛 fix incorrect indicator placement when displaytitle is used

--- includes/Components/CitizenComponentPageHeading.php
+++ includes/Components/CitizenComponentPageHeading.php
@@ -147,7 +147,7 @@ class CitizenComponentPageHeading implements CitizenComponent {
 			// Look for the </span> or </h1> to ensure that it is the last parenthesis of the title
 			// </h1> occurs when the title is a displaytitle
 			$pattern = '/\s?(\p{Ps}.+\p{Pe})<\/(span|h1)>/';
-			$replacement = ' <span class="mw-page-title-parenthesis">$1</span></span>';
+			$replacement = ' <span class="mw-page-title-parenthesis">$1</span></$2>';
 			$titleHtml = preg_replace( $pattern, $replacement, $this->titleData );
 		}
 		return $titleHtml;
