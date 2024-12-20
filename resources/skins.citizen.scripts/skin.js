a6ba1e37072981f268cc0eaa002b84207f55ecdd
alistair3149 <alistair31494322@gmail.com>
2024-12-20 15:56:44 -0500

    feat(wikitables): ✨ add sticky table header

--- resources/skins.citizen.scripts/skin.js
+++ resources/skins.citizen.scripts/skin.js
@@ -55,12 +55,15 @@ function registerServiceWorker() {
 function initBodyContent( bodyContent ) {
 	const
 		sections = require( './sections.js' ),
-		overflowElements = require( './overflowElements.js' );
+		overflowElements = require( './overflowElements.js' ),
+		wikitables = require( './wikitables.js' );
 
 	// Collapsable sections
 	sections.init( bodyContent );
 	// Overflow element enhancements
 	overflowElements.init( bodyContent );
+	// Wikitables enhancements
+	wikitables.init( bodyContent );
 }
 
 /**
