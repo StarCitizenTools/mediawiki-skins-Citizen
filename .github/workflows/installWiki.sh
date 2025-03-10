cfecd68fc76bfeb855d90fcb230017522e7c783f
alistair3149 <alistair31494322@gmail.com>
2025-03-10 13:24:32 -0400

    ci: 👷 update MediaWiki installation and test matrix scripts

--- .github/workflows/installWiki.sh
+++ .github/workflows/installWiki.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-MW_BRANCH=$1
-EXTENSION_NAME=$2
+set -e
 
-wget https://github.com/wikimedia/mediawiki/archive/$MW_BRANCH.tar.gz -nv
+MW_BRANCH=$1
+
+wget "https://github.com/wikimedia/mediawiki/archive/refs/heads/$MW_BRANCH.tar.gz" -nv
 
 tar -zxf $MW_BRANCH.tar.gz
 mv mediawiki-$MW_BRANCH mediawiki
@@ -13,24 +14,11 @@ cd mediawiki
 composer install
 php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
 
-# echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
-# echo 'ini_set("display_errors", 1);' >> LocalSettings.php
-echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
-echo '$wgShowDBErrorBacktrace = true;' >> LocalSettings.php
-echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
-
-echo 'wfLoadSkin( "Citizen" );' >> LocalSettings.php
-
-cat <<EOT >> composer.local.json
-{
-  "require": {
-
-  },
-	"extra": {
-		"merge-plugin": {
-			"merge-dev": true,
-			"include": []
-		}
-	}
-}
+cat <<'EOT' >> LocalSettings.php
+error_reporting(E_ALL| E_STRICT);
+ini_set("display_errors", "1");
+$wgShowExceptionDetails = true;
+$wgShowDBErrorBacktrace = true;
+$wgDevelopmentWarnings = true;
+wfLoadSkin( "Citizen" );
 EOT
