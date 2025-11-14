8a8ecaf83d4cb16137263e4f67c53c87120ded68
alistair3149 <alistair31494322@gmail.com>
2025-11-13 22:09:35 -0500

    refactor(core): ♻️ remove redundant license headder from multiple files
    We already have the license in root directory. For consistency, let's drop all the headers.

--- includes/Partials/Theme.php
+++ includes/Partials/Theme.php
@@ -2,28 +2,6 @@
 
 declare( strict_types=1 );
 
-/**
- * Citizen - A responsive skin developed for the Star Citizen Wiki
- *
- * This file is part of Citizen.
- *
- * Citizen is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Citizen is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Citizen.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @file
- * @ingroup Skins
- */
-
 namespace MediaWiki\Skins\Citizen\Partials;
 
 const CLIENTPREFS_THEME_MAP = [
