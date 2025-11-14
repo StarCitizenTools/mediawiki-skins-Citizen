96a13eba5c4d02161cd8a0238ccf6c6d36b4168c
alistair3149 <alistair31494322@gmail.com>
2025-11-13 20:50:30 -0500

    test(component): 🚨 add unit tests for StickyHeader

---
+++ tests/phpunit/Unit/Components/CitizenComponentStickyHeaderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Skins\Citizen\Tests\Unit\Components;
+
+use MediaWiki\Skins\Citizen\Components\CitizenComponentStickyHeader;
+use MediaWikiUnitTestCase;
+
+/**
+ * @group Citizen
+ * @group Components
+ * @coversDefaultClass \MediaWiki\Skins\Citizen\Components\CitizenComponentStickyHeader
+ */
+class CitizenComponentStickyHeaderTest extends MediaWikiUnitTestCase {
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::getIconButtons
+	 * @covers ::getTemplateData
+	 * @dataProvider provideVisualEditorTabPosition
+	 */
+	public function testGetTemplateData( bool $veTabFirst, string $expectedFirstEditIcon, string $expectedSecondEditIcon ) {
+		$header = new CitizenComponentStickyHeader( $veTabFirst );
+		$data = $header->getTemplateData();
+
+		$this->assertArrayHasKey( 'array-icon-buttons', $data );
+		$buttons = $data['array-icon-buttons'];
+		$this->assertCount( 8, $buttons, 'There should be 8 icon buttons' );
+
+		// Verify button properties for the first button as a sample check
+		$firstButton = $buttons[0];
+		$this->assertEquals( 'quiet', $this->getButtonProperty( $firstButton['class'], 'weight' ) );
+		$this->assertEquals( 'large', $this->getButtonProperty( $firstButton['class'], 'size' ) );
+		$this->assertStringContainsString( 'cdx-button--icon-only', $firstButton['class'] );
+		$this->assertContains( [ 'key' => 'tabindex', 'value' => '-1' ], $firstButton['array-attributes'] );
+
+		// Verify the order of edit icons
+		$this->assertSame( $expectedFirstEditIcon, $buttons[2]['icon'] );
+		$this->assertSame( $expectedSecondEditIcon, $buttons[3]['icon'] );
+	}
+
+	public static function provideVisualEditorTabPosition(): array {
+		return [
+			'VE tab first' => [ true, 'wikimedia-edit', 'wikimedia-wikiText' ],
+			'Wikitext tab first (default)' => [ false, 'wikimedia-wikiText', 'wikimedia-edit' ],
+		];
+	}
+
+	/**
+	 * Helper function to extract button property from class string.
+	 * e.g., 'cdx-button--weight-quiet' -> 'quiet'
+	 */
+	private function getButtonProperty( string $class, string $property ): string {
+		$matches = [];
+		preg_match( "/cdx-button--{$property}-(\w+)/", $class, $matches );
+		return $matches[1] ?? '';
+	}
+}
