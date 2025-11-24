3cd04e0a5dd4e4c5c6ded189c9a59d3c4219a412
alistair3149 <alistair31494322@gmail.com>
2025-11-24 13:26:35 -0500

    ci(lint): 👷 mark CI as failed when it doesn't pass lint steps

--- bin/phpcbf.sh
+++
@@ -1,8 +0,0 @@
-# Force phpcbf to exit with zero
-# See https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927
-
-root=$( dirname $0 )/..
-
-$root/vendor/bin/phpcbf
-
-exit 0
