f2d6e2dacbc1fd1dfda5723c0fe171fe51c42b86
alistair3149 <alistair31494322@gmail.com>
2025-11-27 22:09:39 -0500

    docs(wiki): 📚️ various minor clean ups

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
-import ChangelogsList from './components/ChangelogsList.vue';
 import type { Theme } from 'vitepress';
 
 export default {
 	extends: DefaultTheme,
-	enhanceApp( { app } ) {
-		app.component( 'ChangelogsList', ChangelogsList );
-	},
 } satisfies Theme;
