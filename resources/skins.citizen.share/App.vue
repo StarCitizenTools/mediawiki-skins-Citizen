04fc8df05d204f782ad54845e008aadb32302282
Wolfite <100052562+wlft@users.noreply.github.com>
2026-03-25 22:42:53 +1000

    feat(share): ✨ only use host and pathname

--- resources/skins.citizen.share/App.vue
+++ resources/skins.citizen.share/App.vue
@@ -12,7 +12,7 @@
 				id="citizen-share-link"
 				type="text"
 				readonly
-				:value="window.location.href">
+				:value="window.location.host + window.location.pathname">
 			<button id="citizen-share-copy-button" @click="copyURL">
 				<svg
 					id="citizen-share-copy-button-icon-static"
@@ -92,7 +92,7 @@
 const { defineComponent } = require( 'vue' );
 
 async function copyURL() {
-	const url = window.location.href;
+	const url = window.location.host + window.location.pathname;
 	try {
 		const link = document.getElementById( 'citizen-share-link' );
 		link.select();
